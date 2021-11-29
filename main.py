a636b7e5459d8a79117b6629c2f60245c318e7b3
malikour <37886457+malikour@users.noreply.github.com>
2021-11-29 15:57:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import logging as logger
 from os import rename
 import os
 from dotenv import load_dotenv
-
-load_dotenv()
-logger.basicConfig(format='%(levelname)s %(module)s %(message)s', level=logger.DEBUG)
+print("test")
+#load_dotenv()
+#logger.basicConfig(format='%(levelname)s %(module)s %(message)s', level=logger.DEBUG)
 
 # Change logging level for some verbose modules
-for module in ['selenium.webdriver.remote.remote_connection', 'selenium', 'urllib3.connectionpool']:
-    logger.getLogger(module).setLevel(logger.INFO)
-xlsx = scrapper.scrape_xlsx()
+#for module in ['selenium.webdriver.remote.remote_connection', 'selenium', 'urllib3.connectionpool']:
+ #   logger.getLogger(module).setLevel(logger.INFO)
+#xlsx = scrapper.scrape_xlsx()
 
-with open('new_edt.xlsx', 'wb') as file:
-    file.write(xlsx)
+#with open('new_edt.xlsx', 'wb') as file:
+ #   file.write(xlsx)
 
-UES = 'gs11, gs15, gs21, gs16'
-calendar = xlsparser.make_ics(xlsx, UES)
-name = 'alt'
-with open(f'calendars/ssi-{name}.ics', 'w') as file:
-    logger.info(f'Write on calendars/{name}.ics')
-    file.write(calendar)
+#UES = 'gs11, gs15, gs21, gs16'
+#calendar = xlsparser.make_ics(xlsx, UES)
+#name = 'alt'
+#with open(f'calendars/ssi-{name}.ics', 'w') as file:
+ #   logger.info(f'Write on calendars/{name}.ics')
+  #  file.write(calendar)
